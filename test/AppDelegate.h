cc4b107c80ccac3625b5ad9f8ab125047c2796bd
zhangkaijun <zkj090403111@163.com>
2015-10-18 20:08:21 +0800

    提交

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Zkj on 15/10/18.
+//  Copyright (c) 2015年 zkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Zkj on 15/10/18.
+//  Copyright (c) 2015年 zkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
